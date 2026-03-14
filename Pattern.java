a1becca0f75bb7a577a444886e18b87712d13365
RD-2000 <divaserutuja7@gmail.com>
2026-03-14 14:57:47 +0530

    Pattern code assignment completed

--- Pattern.java
+++ Pattern.java
@@ -2,7 +2,6 @@ public class Pattern {
     public static void main(String[] args) {
 
         int rows = 6;
-
         for (int i = 1; i <= rows; i++) {
 
             for (int j = 1; j <= i; j++) {
